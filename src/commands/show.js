c7c46565c821fcda0735e32f5ebdec0ef45af3c6
alenkart <alenkart.r@gmail.com>
2020-10-21 21:12:32 -0400

    Fixed the show command

--- src/commands/show.js
+++ src/commands/show.js
@@ -7,6 +7,24 @@ function soundToString(sound) {
   return `🔊 ${sound.soundId} 🔗 ${sound.soundUrl}`;
 }
 
+async function sendByGroup(message, sounds) {
+  const messageSize = 1800;
+  const groupSize = 10;
+  const size = messageSize / groupSize;
+
+  let groups = sounds.map(sound => {
+    return soundToString(sound).slice(0, size);
+  });
+
+  for(let i = 0; i <= groups.length; i+=groupSize) {
+    let j = groupSize + i;
+    j = i > groupSize.length ? groupSize.length : j;
+
+    const payload = groups.slice(i, j).join('\n');
+    await message.channel.send(payload);
+  }
+}
+
 command.execute = async function (message, args) {
   const guildId = message.guild.id;
   const [soundId] = args;
@@ -22,15 +40,12 @@ command.execute = async function (message, args) {
     raw: true,
   });
 
-  let messageStr = 'I found nothing';;
-
-  if (sounds.length == 1) {
-    messageStr = soundToString(sounds[0]);
-  } else if (sounds.length > 1) {
-    messageStr = sounds.map(soundToString).join('\n');
+  if (sounds.length < 1) {
+    message.channel.send('I found nothing');
+    return;
   }
 
-  message.channel.send(messageStr);
+  await sendByGroup(message, sounds);
 }
 
 module.exports = command;
